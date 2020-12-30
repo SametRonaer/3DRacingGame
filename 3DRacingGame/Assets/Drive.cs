6dfa6ceeed50c7fce5fa13434da5a8046e31853f
SametWindows <s.ronaer@hotmail.com>
2020-12-30 18:55:33 +0300

    Driving mechanism added.

---
+++ 3DRacingGame/Assets/Drive.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Drive : MonoBehaviour
+{
+    public WheelCollider[] wcs;
+    public GameObject[] wheels;
+    public float torque = 200;
+    public float maxSteerAngle = 30;
+    public float maxBrakeTorque = 500;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float a = Input.GetAxis("Vertical");
+        float s = Input.GetAxis("Horizontal");
+        float b = Input.GetAxis("Jump");
+        Go(a , s , b);
+    }
+    
+    void Go(float accel , float steer, float brake)
+    {
+        accel = Mathf.Clamp(accel, -1, 1);
+        steer = Mathf.Clamp(steer, -1, 1) * maxSteerAngle;
+        brake = Mathf.Clamp(brake, 0, 1) * maxBrakeTorque;
+        float thrussTorque = accel * torque;
+        for(int i = 0; i < 4; i++){
+            wcs[i].motorTorque = thrussTorque;
+            if (i < 2)
+            {
+            wcs[i].steerAngle = steer;
+            }
+            else
+            {
+                wcs[i].brakeTorque = brake;
+            }
+
+            Quaternion quat;
+            Vector3 position;
+            wcs[i].GetWorldPose(out position, out quat);
+            wheels[i].transform.position = position;
+            wheels[i].transform.rotation = quat;
+        }
+
+      
+        
+    }
+}
