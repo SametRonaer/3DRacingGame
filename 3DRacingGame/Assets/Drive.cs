943cc559d46d02ebb4e078ede0843b75e1f382e7
SametWindows <s.ronaer@hotmail.com>
2021-01-01 12:03:25 +0300

    Sound and smoke effects added.

--- 3DRacingGame/Assets/Drive.cs
+++ 3DRacingGame/Assets/Drive.cs
@@ -10,27 +10,71 @@ public class Drive : MonoBehaviour
     public float maxSteerAngle = 30;
     public float maxBrakeTorque = 500;
 
-    // Start is called before the first frame update
+    public AudioSource skidSound;
+    public AudioSource highAccel;
+
+    public Transform SkidTrailPrefab;
+    Transform[] skidTrails = new Transform[4];
+   
+    public ParticleSystem smokePrefab;
+    ParticleSystem[] skidSmoke = new ParticleSystem[4];
+
+    public GameObject brakeLight;
+
+    public Rigidbody rb;
+    public float gearLength = 3;
+    public float currentSpeed { get { return rb.velocity.magnitude * gearLength; } }
+
+    public float lowPitch = 1f;
+    public float highPitch = 6f;
+    public int numGears = 5;
+    float rpm;
+    int currentGear = 1;
+    float currentGearPerc;
+    public float maxSpeed = 200f;
+
+
+
     void Start()
     {
-       
+       for(int i= 0; i<4; i++)
+        {
+            skidSmoke[i] = Instantiate(smokePrefab);
+            skidSmoke[i].Stop();
+        }
+        brakeLight.SetActive(false);
     }
-
-    // Update is called once per frame
     void Update()
     {
         float a = Input.GetAxis("Vertical");
         float s = Input.GetAxis("Horizontal");
         float b = Input.GetAxis("Jump");
         Go(a , s , b);
+
+        CheckForSkid();
+        CalculateEngineSound();
     }
-    
     void Go(float accel , float steer, float brake)
     {
         accel = Mathf.Clamp(accel, -1, 1);
         steer = Mathf.Clamp(steer, -1, 1) * maxSteerAngle;
         brake = Mathf.Clamp(brake, 0, 1) * maxBrakeTorque;
-        float thrussTorque = accel * torque;
+       
+        if(brake != 0)
+        {
+            brakeLight.SetActive(true);
+        }
+        else
+        {
+            brakeLight.SetActive(false);
+        }
+
+        float thrussTorque = 0;
+        if(currentSpeed < maxSpeed)
+        {
+            thrussTorque = accel * torque;
+        }
+
         for(int i = 0; i < 4; i++){
             wcs[i].motorTorque = thrussTorque;
             if (i < 2)
@@ -52,4 +96,83 @@ public class Drive : MonoBehaviour
       
         
     }
+
+    void CheckForSkid()
+    {
+        int numSkidding = 0;
+        for(int i = 0; i<4; i++)
+        {
+            WheelHit wheelHit;
+            wcs[i].GetGroundHit(out wheelHit);
+            if(Mathf.Abs(wheelHit.forwardSlip) >= 0.4f || Mathf.Abs(wheelHit.sidewaysSlip) >= 0.4f)
+            {
+                numSkidding++;
+                if (!skidSound.isPlaying)
+                {
+                    skidSound.Play();
+                }
+                StartSkidTrail(i);
+                skidSmoke[i].transform.position = wcs[i].transform.position - wcs[i].transform.up * wcs[i].radius;
+                skidSmoke[i].Emit(1);
+            }
+            else
+            {
+                EndSkidTrail(i);
+            }
+        }
+
+        if(numSkidding == 0 && skidSound.isPlaying)
+        {
+            skidSound.Stop();
+         
+        }
+    }
+    void CalculateEngineSound()
+    {
+        float gearPercentage = (1 / (float)numGears);
+        float targetGearFactor = Mathf.InverseLerp(gearPercentage * currentGear, gearPercentage * (currentGear + 1), Mathf.Abs(currentSpeed / maxSpeed));
+        currentGearPerc = Mathf.Lerp(currentGearPerc, targetGearFactor, Time.deltaTime * 5f);
+
+        var gearNumFactor = currentGear / (float) numGears;
+        rpm = Mathf.Lerp(gearNumFactor, 1, currentGearPerc);
+        float speedPercentage = Mathf.Abs(currentSpeed / maxSpeed);
+        float upperGearMax = (1 / (float)numGears) * (currentGear + 1);
+        float downGearMax = (1 / (float)numGears) * currentGear;
+
+        if(currentGear>0 && speedPercentage< downGearMax)
+        {
+            currentGear--;
+        } 
+        if(speedPercentage > upperGearMax && (currentGear < (numGears - 1)))
+        {
+            currentGear++;
+        }
+
+        float pitch = Mathf.Lerp(lowPitch, highPitch, rpm);
+        highAccel.pitch = Mathf.Min(highPitch, pitch) * 0.25f;
+    }
+    public void StartSkidTrail(int i)
+    {
+        if(skidTrails[i] == null)
+        {
+            skidTrails[i] = Instantiate(SkidTrailPrefab);
+        }
+        skidTrails[i].parent = wcs[i].transform;
+        skidTrails[i].localPosition = -Vector3.up * wcs[i].radius;
+        skidTrails[i].localRotation = Quaternion.Euler(90, 0, 0);
+    }
+    public void EndSkidTrail(int i)
+    {
+        if (skidTrails[i] == null) return;
+        Transform holder = skidTrails[i];
+        skidTrails[i] = null;
+        holder.parent = null;
+        holder.rotation = Quaternion.Euler(90, 0, 0);
+        Destroy(holder.gameObject, 30f);
+    }
+
+
+
+   
+
 }
