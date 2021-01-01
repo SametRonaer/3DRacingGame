23d29891af991a06c5459fcc5dfee18bea09f0fe
SametWindows <s.ronaer@hotmail.com>
2021-01-01 18:00:20 +0300

    Trail size and sound fixed

--- 3DRacingGame/Assets/Drive.cs
+++ 3DRacingGame/Assets/Drive.cs
@@ -28,11 +28,14 @@ public class Drive : MonoBehaviour
     public float lowPitch = 1f;
     public float highPitch = 6f;
     public int numGears = 5;
-    float rpm;
-    int currentGear = 1;
-    float currentGearPerc;
+    //float rpm;
+    //int currentGear = 1;
+    //float currentGearPerc;
     public float maxSpeed = 200f;
 
+    int gearStatus = 1;
+    float gearAmount = 50;
+    [SerializeField] GameObject smokeEffect;
 
 
     void Start()
@@ -50,40 +53,38 @@ public class Drive : MonoBehaviour
         float s = Input.GetAxis("Horizontal");
         float b = Input.GetAxis("Jump");
         Go(a , s , b);
+        SetSound();
+       // SetSmoke();
+       // print(currentSpeed);
 
         CheckForSkid();
-        CalculateEngineSound();
+        //CalculateEngineSound();
     }
     void Go(float accel , float steer, float brake)
     {
         accel = Mathf.Clamp(accel, -1, 1);
         steer = Mathf.Clamp(steer, -1, 1) * maxSteerAngle;
         brake = Mathf.Clamp(brake, 0, 1) * maxBrakeTorque;
-       
-        if(brake != 0)
-        {
-            brakeLight.SetActive(true);
-        }
-        else
-        {
-            brakeLight.SetActive(false);
-        }
+
+        //SetStopLight(brake);
 
         float thrussTorque = 0;
-        if(currentSpeed < maxSpeed)
+        if (currentSpeed < maxSpeed)
         {
             thrussTorque = accel * torque;
         }
 
-        for(int i = 0; i < 4; i++){
+        for (int i = 0; i < 4; i++)
+        {
             wcs[i].motorTorque = thrussTorque;
             if (i < 2)
             {
-            wcs[i].steerAngle = steer;
+                wcs[i].steerAngle = steer;
+                wcs[i].brakeTorque = brake;
             }
             else
             {
-                wcs[i].brakeTorque = brake;
+               // wcs[i].brakeTorque = brake;
             }
 
             Quaternion quat;
@@ -92,11 +93,70 @@ public class Drive : MonoBehaviour
             wheels[i].transform.position = position;
             wheels[i].transform.rotation = quat;
         }
-
-      
-        
     }
 
+
+   
+
+
+    private void SetStopLight(float brake)
+    {
+        if (brake != 0)
+        {
+            brakeLight.SetActive(true);
+        }
+        else
+        {
+            brakeLight.SetActive(false);
+        }
+    }
+
+
+    void SetSound()
+    {
+        float speedRatio = (currentSpeed / gearAmount) + 0.6f;
+       
+      
+        if(speedRatio < 1.6f)
+        {
+            highAccel.pitch = speedRatio;
+        }else if(gearStatus < 5)
+        {
+            gearStatus++;
+            gearAmount += 50;
+        }
+    } // Fix gear down mechanism
+
+   
+
+
+
+    //void CalculateEngineSound()
+    //{
+    //    float gearPercentage = (1 / (float)numGears);
+    //    float targetGearFactor = Mathf.InverseLerp(gearPercentage * currentGear, gearPercentage * (currentGear + 1), Mathf.Abs(currentSpeed / maxSpeed));
+    //    currentGearPerc = Mathf.Lerp(currentGearPerc, targetGearFactor, Time.deltaTime * 5f);
+
+    //    var gearNumFactor = currentGear / (float) numGears;
+    //    rpm = Mathf.Lerp(gearNumFactor, 1, currentGearPerc);
+    //    float speedPercentage = Mathf.Abs(currentSpeed / maxSpeed);
+    //    float upperGearMax = (1 / (float)numGears) * (currentGear + 1);
+    //    float downGearMax = (1 / (float)numGears) * currentGear;
+
+    //    if(currentGear>0 && speedPercentage< downGearMax)
+    //    {
+    //        currentGear--;
+    //    } 
+    //    if(speedPercentage > upperGearMax && (currentGear < (numGears - 1)))
+    //    {
+    //        currentGear++;
+    //    }
+
+    //    float pitch = Mathf.Lerp(lowPitch, highPitch, rpm);
+    //    highAccel.pitch = Mathf.Min(highPitch, pitch) * 0.25f;
+    //}
+   
+    
     void CheckForSkid()
     {
         int numSkidding = 0;
@@ -104,7 +164,7 @@ public class Drive : MonoBehaviour
         {
             WheelHit wheelHit;
             wcs[i].GetGroundHit(out wheelHit);
-            if(Mathf.Abs(wheelHit.forwardSlip) >= 0.4f || Mathf.Abs(wheelHit.sidewaysSlip) >= 0.4f)
+            if(Mathf.Abs(wheelHit.forwardSlip) >= 0.7f || Mathf.Abs(wheelHit.sidewaysSlip) >= 0.9f)
             {
                 numSkidding++;
                 if (!skidSound.isPlaying)
@@ -112,12 +172,14 @@ public class Drive : MonoBehaviour
                     skidSound.Play();
                 }
                 StartSkidTrail(i);
+              
                 skidSmoke[i].transform.position = wcs[i].transform.position - wcs[i].transform.up * wcs[i].radius;
                 skidSmoke[i].Emit(1);
             }
             else
             {
                 EndSkidTrail(i);
+              
             }
         }
 
@@ -126,30 +188,6 @@ public class Drive : MonoBehaviour
             skidSound.Stop();
          
         }
-    }
-    void CalculateEngineSound()
-    {
-        float gearPercentage = (1 / (float)numGears);
-        float targetGearFactor = Mathf.InverseLerp(gearPercentage * currentGear, gearPercentage * (currentGear + 1), Mathf.Abs(currentSpeed / maxSpeed));
-        currentGearPerc = Mathf.Lerp(currentGearPerc, targetGearFactor, Time.deltaTime * 5f);
-
-        var gearNumFactor = currentGear / (float) numGears;
-        rpm = Mathf.Lerp(gearNumFactor, 1, currentGearPerc);
-        float speedPercentage = Mathf.Abs(currentSpeed / maxSpeed);
-        float upperGearMax = (1 / (float)numGears) * (currentGear + 1);
-        float downGearMax = (1 / (float)numGears) * currentGear;
-
-        if(currentGear>0 && speedPercentage< downGearMax)
-        {
-            currentGear--;
-        } 
-        if(speedPercentage > upperGearMax && (currentGear < (numGears - 1)))
-        {
-            currentGear++;
-        }
-
-        float pitch = Mathf.Lerp(lowPitch, highPitch, rpm);
-        highAccel.pitch = Mathf.Min(highPitch, pitch) * 0.25f;
     }
     public void StartSkidTrail(int i)
     {
