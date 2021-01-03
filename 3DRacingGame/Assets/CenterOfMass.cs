e510ffb7be065fbd21f0a49a6af1c55ed089d7ae
SametWindows <s.ronaer@hotmail.com>
2021-01-03 17:11:45 +0300

    Center of mass added.

---
+++ 3DRacingGame/Assets/CenterOfMass.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CenterOfMass : MonoBehaviour
+{
+
+    [SerializeField] GameObject COM;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Rigidbody>().centerOfMass = COM.transform.localPosition;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
